012503d56a3520fcfe773ee38966d3b32aac7028
hamdy.ali <hamdydraw@gmail.com>
2025-10-06 07:19:18 +0400

    feat: Add PowerShell script for downloading NuGet packages and include nuget.exe

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  base: "/pmaweb/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
