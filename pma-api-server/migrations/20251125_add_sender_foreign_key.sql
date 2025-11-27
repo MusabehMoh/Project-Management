6f12bdf378ac8a20dc32fca19daa09047b15f651
hamdy.ali <hamdydraw@gmail.com>
2025-11-27 11:22:32 +0400

    fix: Corrected the Name value for RequirementStatus in Lookups insertion

--- pma-api-server/migrations/20251125_add_sender_foreign_key.sql
+++ pma-api-server/migrations/20251125_add_sender_foreign_key.sql
@@ -4,7 +4,7 @@ ADD SentBy INT
 
   SET IDENTITY_INSERT [dbo].[Lookups] ON
   INSERT [dbo].[Lookups] ([Id], [Code], [Value], [Name], [NameAr], [Order], [IsActive], [CreatedAt], [UpdatedAt]) VALUES (26, N'RequirementStatus', 9, N'ReturnedToAnalyst', N'معاد إلى المحلل', 9, 1, NULL, NULL)
-    INSERT [dbo].[Lookups] ([Id], [Code], [Value], [Name], [NameAr], [Order], [IsActive], [CreatedAt], [UpdatedAt]) VALUES (27, N'RequirementStatus', 10, N'ReturnedToAnalyst', N'معاد إلى المدير', 10, 1, NULL, NULL)
+    INSERT [dbo].[Lookups] ([Id], [Code], [Value], [Name], [NameAr], [Order], [IsActive], [CreatedAt], [UpdatedAt]) VALUES (27, N'RequirementStatus', 10, N'ReturnedToManager', N'معاد إلى المدير', 10, 1, NULL, NULL)
 	SET IDENTITY_INSERT [dbo].[Lookups] OFF
 
     update [Lookups] set NameAr= N'عادي' where id=13
